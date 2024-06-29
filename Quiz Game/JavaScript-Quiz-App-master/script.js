98501b6dcb6a3175b1ea1911f795f23aefa4afe2
ankitaa090103 <133684047+ankitaa090103@users.noreply.github.com>
2024-06-29 13:06:02 +0530

    Update script.js

--- Quiz Game/JavaScript-Quiz-App-master/script.js
+++ Quiz Game/JavaScript-Quiz-App-master/script.js
@@ -1,110 +1,135 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
 const questions = [
-  {
-    question: 'What is 2 + 2?',
-    answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
+    {
+
+    question: "What does HTML stand for?",
+     answer: [
+      {text:"Hyper Text Preprocessor",correct:false},
+      {text:"Hyper Text Markup Language",correct:true},
+      {text:"Hyper Text Multiple Language",correct:false},
+      {text:"Hyper Tool Multi Language",correct:false},
     ]
   },
-  {
-    question: 'Who is the best YouTuber?',
-    answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
+    {
+ 
+    question: "What does CSS stand for?",
+    answer: [
+      {text:"Common Style Sheet",correct:false},
+      {text:"Colorful Style Sheet",correct:false},
+      {text:"Computer Style Sheet",correct:false},
+      {text:"Cascading Style Sheet",correct:true},
     ]
   },
-  {
-    question: 'Is web development fun?',
-    answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
+    {
+
+    question: "What does PHP stand for?",
+    answer:[
+      {text:"Hypertext Preprocessor",correct:true},
+      {text:"Hypertext Programming",correct:false},
+      {text:"Hypertext Preprogramming",correct:false},
+      {text:"Hometext Preprocessor",correct:false},
     ]
   },
-  {
-    question: 'What is 4 * 2?',
-    answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+    {
+
+    question: "What does SQL stand for?",
+    answer: [
+      {text:"Stylish Question Language",correct:false},
+      {text:"Stylesheet Query Language",correct:false},
+      {text:"Statement Question Language",correct:false},
+      {text:"Structured Query Language",correct:true},
     ]
-  }
-]
+  },
+    {
+    
+    question: "What does XML stand for?",
+    answer:[
+      {text:"eXtensible Markup Language",correct:true},
+      {text:"eXecutable Multiple Language",correct:false},
+      {text:"eXTra Multi-Program Language",correct:false},
+      {text:"eXamine Multiple Language",correct:false},
+    ]
+  },
+];
+
+const questionelement =document.getElementById("questions");
+const answerbuttons =document.getElementById("answer-buttons");
+const nextbutton =document.getElementById("next-btn");
+
+//when ever the game gets started the score and the question number gets increses
+let currentquestionindex =0;
+let score=0;
+
+function startquiz()
+{
+    currentquestionindex=0;
+    score=0;
+    nextbutton.innerHTML="Next"
+    showquestion();
+}
+function showquestion(){
+    resetState();
+    let currentquestion = questions[currentquestionindex];
+    let questionNo=currentquestionindex + 1;
+    questionelement.innerHTML=questionNo + "." + currentquestion.question;
+    currentquestion.answer.forEach(answers=>{
+        const button = document.createElement("button");
+        button.innerHTML=answers.text;
+        button.classList.add("btn");
+        answerbuttons.appendChild(button);
+        if(answers.correct.correct)
+            {
+                button.dataset.correct=answers.correct;
+            }
+        button.addEventListener("click",selectanswer);
+    });
+}
+function resetState(){
+    nextbutton.style.display="none";
+    while(answerbuttons.firstChild){
+        answerbuttons.removeChild(answerbuttons.firstChild);
+    }
+}
+function selectanswer(e)
+{
+    const selectedbtn =e.target;
+    const iscorrect = selectedbtn.dataset.correct==="true";
+    if(iscorrect){
+            selectedbtn.classList.add("correct");
+            score++;
+        }else{
+            selectedbtn.classList.add("incorrect");
+        }
+        Array.from(answerbuttons.children).forEach(button =>
+            {
+                if(button.dataset.correct === "true")
+                    {
+                        button.classList.add("correct");
+                    }
+                    button.disabled = true;
+            });
+            nextbutton.style.display="block";
+}
+function showscore(){
+    resetState();
+    questionelement.innerHTML=`Your Scored ${score} out of ${questions.length}!!!`;
+    nextbutton.innerHTML="play again";
+    nextbutton.style.display="block";
+}
+function handlenextbutton(){
+    currentquestionindex++;
+    if(currentquestionindex<questions.length){
+        showquestion();
+    }
+    else{
+        showscore();
+    }
+}
+nextbutton.addEventListener("click",()=>{
+    if(currentquestionindex<questions.length){
+        handlenextbutton();
+    }
+    else{
+        startquiz();
+    }
+})
+startquiz();
